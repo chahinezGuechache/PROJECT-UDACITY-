efd7563778bdb3b2b1f03f715587163ba7fd0f37
chahinezGuechache <chahineznouza@gmail.com>
2021-08-22 18:41:27 +0100

    Add files via upload

---
+++ src/server.ts
@@ -0,0 +1,86 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+  app.get( "/filteredimage", (req: Request, res: Response) => {
+  //  1. validate the image_url query
+     // Get our image_url from the query
+     const image_url  = req.query.image_url;
+
+     // Check if the image_url exists within the query or not 
+    if (!image_url) {
+      res.send( `The URL of the image is needed ,${image_url}`);
+      return;
+    }
+    // Test if the URL really exists 
+     try {
+      let existance=new URL(image_url);
+
+     if (existance) {
+       res.status(200).send( `The URL of the image EXIST `)
+    }
+      }
+      catch(err) {
+           return res.status(404).send( `The URL of the image doesn't EXIST `);}
+
+   
+
+  //2. call filterImageFromURL(image_url) to filter the image  
+  let thefiltredImage: Promise<string> = filterImageFromURL(image_url);
+  
+  
+  thefiltredImage.then(
+    myimage => {
+      //3. send the resulting file in the response 
+      res.sendFile(myimage, () => {
+      //4. deletes any files on the server on finish of the response
+      let TheDeletedImage: Array<string> = new Array(myimage);
+      deleteLocalFiles(TheDeletedImage);});
+      }).catch(error => {
+        res.status(422).send(`The image was not found`);
+        return;})
+     } );
+  
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
